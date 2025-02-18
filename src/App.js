1608a79c203af3ff01c61afd1e7f47194457ab3f
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2025-02-18 12:18:39 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
-import React from "react";
-import "./index.css";
-import sideimg from "./img/IMAGE NUMERO 1 3.png"
-// import CenteredHead from "./centeredhead";
-import CenteredHeading from "./centeredhead";
-import { FaSearch} from "react-icons/fa"
+import React from 'react';
+import Header from './components/header1'; // Importing the Header component
+import "./App.scss"
 
-// import { Link } from 'react-router-dom';
-
-const Header = () => {
-  return (
-    <header className="header">
-    <div className="logo"><FaSearch></FaSearch> SEARCH</div>
-    <nav className="nav">
-      <span className="nav-button">Since 1973</span>
-      <span className="nav-button">Services</span>
-      <span className="nav-button">Experience</span>
-      <span className="nav-button">Shop</span>
-      <span className="nav-button">Project X</span>
-    </nav>
-    <div className="user-actions">
-      <span className="btn">My KB</span>
-    </div>
-  </header>
-  );
-};
-
-const HeroSection = () => {
-  return (
-    <section className="hero">
-      <div className="hero-text">
-        <h1>FOUNDER’S STYLE SINCE</h1>
-        <h2>1973</h2>
-        <p>A lascivious wardrobe statement that can never lose its power.</p>
-        <button className="btn">Book an appointment</button>
-      </div>
-      <div className="hero-image">
-      <img src={sideimg} alt="Logo" />
-      </div>
-    </section>
-  );
-};
-
-const App = () => {
+function App() {
   return (
     <div>
-      <CenteredHeading/>
-      <Header />
-      <HeroSection />
+      <Header /> 
+     
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/header1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./header1.scss"
+
+function Header() {
+  return (
+    <header>
+      <h1 className="testlogo">TEST LOGO</h1>
+    </header>
+  );
+}
+
+export default Header;
